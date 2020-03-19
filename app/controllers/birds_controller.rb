7026f71e41f4649c448c0ae16914fe0d8f1bcf28
github username <github email address>
2020-03-18 22:59:59 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,19 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
   end
+ 
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: birds, only: [:id, :name, :species]
+    # render json: birds, except: [:created_at, :updated_at]
+    # render json: birds.to_json(except: [:created_at, :updated_at])
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
+  end
+
 end
